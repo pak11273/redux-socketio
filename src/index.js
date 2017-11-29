9afb98dc1b5c9461014669cac3ee019ec5e6ac7c
pak11273 <pak11273@gmail.com>
2017-11-29 01:52:24 +0000

    babelify index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+export default store => {
+  return ({dispatch, getState}) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState)
+    }
+
+    const {promise, type, types, ...rest} = action
+
+    if (type === 'socket') {
+      return next(action)
+    }
+
+    const [REQUEST, SUCCESS, FAILURE] = types
+
+    next({...rest, type: REQUEST})
+
+    return promise(socket)
+      .then(result => {
+        return next({...rest, result, type: SUCCESS})
+      })
+      .catch(error => {
+        return next({...rest, error, type: FAILURE})
+      })
+  }
+}
